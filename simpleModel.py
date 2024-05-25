21c14717b4e6230ffa00882e5f69ef69ccb62b99
TK <tingkaihsu0924@gmail.com>
2024-05-25 10:13:46 +0800

    simple model and training

--- simpleModel.py
+++ simpleModel.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 from tensorflow import keras
-class _DotDict():
-    pass
 
-def simple_model_base(features=None, setting=None, name='Simple Model'):
+class _DotDict:
+    def __init__(self, num_class):
+        self.num_class = num_class
+
+def simple_model_base(features=None, setting=None, name='Simple_Model'):
     with tf.name_scope(name):
         out = keras.layers.Dense(setting.num_class, activation='linear')(features)
         return out
     
 def get_simple_model(num_classes, input_shapes):
-    setting = _DotDict()
-    setting.num_classes = num_classes
+    setting = _DotDict(num_class=num_classes)
     features = keras.Input(name='features', shape=input_shapes['features']) if 'features' in input_shapes else None
     outputs = simple_model_base(features, setting)
-    return keras.Model(inputs=[features], outputs=outputs, name='Simple Model')
+    return keras.Model(inputs=[features], outputs=outputs, name='Simple_Model')
 
